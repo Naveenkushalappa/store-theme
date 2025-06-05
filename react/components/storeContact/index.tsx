d976672ef49fd8dbd283781ee37f4248a0028e93
Naveenkushalappa <naveen.k@trikatechnologies.com>
2025-06-05 12:19:30 +0530

    Add storeContact block and route.

---
+++ react/components/storeContact/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StoreContact = () => {
+    return (
+        <div>
+            <h3>Store Contact form coming soon</h3>
+        </div>
+    )
+}
+
+export default StoreContact;
